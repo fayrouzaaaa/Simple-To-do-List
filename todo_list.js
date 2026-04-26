524a29687e7c0418ed2d605ac19418a59f62301e
fayrouzaaaa <fayrouzahossam@gmail.com>
2026-04-27 00:04:00 +0200

    Remove function

--- todo_list.js
+++ todo_list.js
@@ -4,6 +4,11 @@ let clearAll = document.getElementById("clearBtn");
 let todoList = document.getElementById("todo");
 let counter = 0;
 let tasks = [];
+if (localStorage.getItem("tasks")){
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+document.onload = displayTasks();
 
 addButton.addEventListener("click", ()=> addTask());
 clearAll.addEventListener("click", ()=> clear());
@@ -15,20 +20,37 @@ function addTask(){
     inputField.value = "";
     tasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    diplayTasks();
+    location.reload();
 }
 
-function diplayTasks(){
+function displayTasks(){
     let html = "";
     for (let i=0; i<tasks.length; i++){
-        html += '<li>' + tasks[i] + ' <button id="editBtn" ><i class="fa-solid fa-pen-to-square"></i></button> <button id="removeBtn"> </button></li>';
+        html += '<li class ="item">' + tasks[i] + 
+        ' <button class="checkBtn" ><i class="fa-solid fa-check checkIcon"></i></button>'+
+        ' <button class="removeBtn"><i class="fa-regular fa-trash-can removeIcon"></i></button></li>';
     }
 
     todoList.innerHTML = html;
+    activateRemoveButtons();
+    //activateCheckButtons();
 }
 
 function clear(){
     todoList.innerHTML= "";
     tasks.length=0;
-    diplayTasks();
+    displayTasks();
+}
+
+function activateRemoveButtons (){
+    let removeButtons = document.querySelectorAll(".removeBtn");
+    removeButtons.forEach((rb, i)=> {
+        rb.addEventListener("click", ()=> {removeTask(i)});
+    })
+}
+
+function removeTask(i){
+    tasks.splice(i,1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    displayTasks();
 }
