cbb2a6ff07417a6f569aa68c6c9b8b83f46e7fd5
fayrouzaaaa <fayrouzahossam@gmail.com>
2026-04-27 01:01:36 +0200

    Adding check functionality

--- todo_list.js
+++ todo_list.js
@@ -4,9 +4,16 @@ let clearAll = document.getElementById("clearBtn");
 let todoList = document.getElementById("todo");
 let counter = 0;
 let tasks = [];
+let checked = [];
 if (localStorage.getItem("tasks")){
     tasks = JSON.parse(localStorage.getItem("tasks"));
 }
+
+if(localStorage.getItem("checked")){
+    checked = JSON.parse (localStorage.getItem("checked"));
+}
+
+localStorage.setItem("checked", JSON.stringify(checked));
 
 document.onload = displayTasks();
 
@@ -20,25 +27,37 @@ function addTask(){
     inputField.value = "";
     tasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    location.reload();
+    displayTasks();
 }
 
 function displayTasks(){
     let html = "";
     for (let i=0; i<tasks.length; i++){
-        html += '<li class ="item">' + tasks[i] + 
-        ' <button class="checkBtn" ><i class="fa-solid fa-check checkIcon"></i></button>'+
+
+        if (checked[i]){
+        html += '<li class ="item checked" id="item' + i + '" >' + tasks[i] + 
+        ' <button class="checkBtn"><i class="fa-solid fa-check checkIcon"></i></button>'+
         ' <button class="removeBtn"><i class="fa-regular fa-trash-can removeIcon"></i></button></li>';
     }
+    else {
+         html += '<li class ="item" id="item' + i + '" >' + tasks[i] + 
+        ' <button class="checkBtn"><i class="fa-solid fa-check checkIcon"></i></button>'+
+        ' <button class="removeBtn"><i class="fa-regular fa-trash-can removeIcon"></i></button></li>';
+    }
+}
+
 
     todoList.innerHTML = html;
     activateRemoveButtons();
-    //activateCheckButtons();
+    activateCheckButtons();
 }
 
 function clear(){
     todoList.innerHTML= "";
     tasks.length=0;
+    checked.length = 0;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("checked", JSON.stringify(checked));
     displayTasks();
 }
 
@@ -52,5 +71,23 @@ function activateRemoveButtons (){
 function removeTask(i){
     tasks.splice(i,1);
     localStorage.setItem("tasks", JSON.stringify(tasks));
+    checked.splice(i,1);
+    localStorage.setItem("checked", JSON.stringify(checked));
+    displayTasks();
+}
+
+function activateCheckButtons(){
+    let checkButtons = document.querySelectorAll(".checkBtn");
+    checkButtons.forEach((cb, i)=>{
+        cb.addEventListener("click", () => {
+            checkTask(i);
+        })
+    })
+}
+
+function checkTask(i){
+    item = document.getElementById("item"+i);
+    checked[i]=true;
+    localStorage.setItem("checked", JSON.stringify(checked));
     displayTasks();
 }
